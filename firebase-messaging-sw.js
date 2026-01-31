2b40bee34a465550c2cabc4fe422bb545500a0e3
apniduniyafamily <apniduniya.family@gmail.com>
2026-01-31 21:43:51 +0530

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts("https://www.gstatic.com/firebasejs/12.6.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.6.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAHAQjn4sTuR9FiGF3ljRrFEMZQjsx-SHg",
+  authDomain: "apni-duniya-b53d7.firebaseapp.com",
+  projectId: "apni-duniya-b53d7",
+  messagingSenderId: "178853058918",
+  appId: "1:178853058918:web:91dfaa5c5e41446da290a7"
+});
+
+firebase.messaging();
